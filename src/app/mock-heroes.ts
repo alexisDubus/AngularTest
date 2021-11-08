ac07a56090ea1597932037f50933a63ab19726fe
Alexis D <alexis.dubus@epsi.fr>
2021-11-08 22:54:36 +0100

    add dashboard, mapping

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Benoit Cavrois' },
+  { id: 12, name: 'Azhek Ahriman' },
+  { id: 13, name: 'Magnus the red' },
+  { id: 14, name: 'Lorgar Aurelian' },
+  { id: 15, name: 'Ohrmuzd Ahriman' },
+  { id: 16, name: 'Akhor\'menet' },
+  { id: 17, name: 'Amon' },
+  { id: 18, name: 'Ctesias' },
+  { id: 19, name: 'Hathor Maat' },
+  { id: 20, name: 'Xepthis' }
+];
